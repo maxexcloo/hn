a7223bdeb5e5078e73b55be5c63c547024b3c923
Max Schaefer <max@excloo.com>
2025-07-27 20:06:44 +1000

    feat: Add basic project structure and list stories

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const hnUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+
+  https.get(hnUrl, (apiRes) => {
+    let data = '';
+    apiRes.on('data', (chunk) => {
+      data += chunk;
+    });
+    apiRes.on('end', () => {
+      const storyIds = JSON.parse(data).slice(0, 30);
+      const storyPromises = storyIds.map(id =>
+        new Promise((resolve, reject) => {
+          https.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, (itemRes) => {
+            let itemData = '';
+            itemRes.on('data', (chunk) => {
+              itemData += chunk;
+            });
+            itemRes.on('end', () => {
+              resolve(JSON.parse(itemData));
+            });
+          }).on('error', (err) => {
+            reject(err);
+          });
+        })
+      );
+
+      Promise.all(storyPromises)
+        .then(stories => {
+          res.render('index', { stories });
+        })
+        .catch(err => {
+          res.status(500).send('Error fetching stories');
+        });
+    });
+  }).on('error', (err) => {
+    res.status(500).send('Error fetching top stories');
+  });
 });
 
 app.listen(port, () => {
